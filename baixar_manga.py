c9efbd0c5f38640c734af5d2ad4f884dae3fdd41
Adelio Fonseca <adeliofonseca.lord@hotmail.com>
2019-11-26 23:12:31 -0100

    Codigo de script beta em portugues

---
+++ baixar_manga.py
@@ -0,0 +1,81 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from progress.bar import Bar
+import shutil
+
+
+link_anime = input('Entre com link do anime: ')
+
+#Selecionar capitulo 
+numero_capitulo = input('Entre com o entervalo dos capitulos separado com  -   :  ')
+comeco_capitulo = -1 * int(numero_capitulo[0])
+fim_capitulo = -1 * int(numero_capitulo[2::])
+
+
+#Iniciar url e fazer soup
+manga = requests.get(link_anime)
+soup = BeautifulSoup(manga.text,'html.parser')
+
+#Apanhar o nome do manga a ser baixado
+nome_anime = soup.body.h1.find_all(string=True) #Apanhar nome anime
+print(str(nome_anime[0]))
+
+#Apanhar os links dos capitulos
+cap_manga = soup.find(class_= "chapter-list")
+capituloss = cap_manga.find_all('a') #Div Link dos capitulos
+
+
+capitulos_link = [] #todos os links dos capitulos
+
+for capitulos in capituloss:
+    capitulos_link.append(str(capitulos.attrs['href'])) 
+
+#Baixando as imagens dos capitulos
+
+
+bar1 = Bar('Baixando', max = len(capitulos_link))
+count = 0
+for capitulo_link in capitulos_link:
+    count += 1
+    capitulo_links = requests.get(capitulo_link)
+    soup_capitulo = BeautifulSoup(capitulo_links.text,'html.parser')
+
+    #Encontrar as imagens
+    class_imagem = soup_capitulo.find(class_= "vung-doc")
+    tag_imagem = class_imagem.find_all('img')
+    imagens = [] #Lista de todas as imagens do capitulo
+    for img in tag_imagem:
+        imagens.append(str(img.attrs['src'])) 
+
+    #Baixando imagem
+    newpath = str(nome_anime[0]) + str(count) + '/'
+    os.makedirs(newpath)
+
+    bar = Bar('Baixando', max = len(imagens))
+    contar_numero = 0
+    for imagem in imagens:
+        contar_numero += 1
+        numero = str(contar_numero)
+        full_path = newpath + str(nome_anime[0]) + numero + '.jpg'
+        response = requests.get(imagem, stream=True)
+        with open(full_path, 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        del response
+        bar.next()
+    bar.finish()
+    bar1.next()
+bar1.finish()
+
+
+    
+
+      
+
+
+
+
+
+
+
+
