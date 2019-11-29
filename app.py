9fb10d50c9ed29cc43b98287cf1d82727223a93d
RaphK20 <rafalopes@live.com>
2019-11-29 11:38:31 -0100

    Translated everything to english

--- app.py
+++ app.py
@@ -8,60 +8,60 @@ from search_manga import chosen_manga
 link_anime = chosen_manga()
 
 #Choose chapters
-numero_capitulo = input('Entre com o entervalo dos capitulos separado com  -  s:  ')
-numero_capitulo = numero_capitulo.split('-',1)
-comeco_capitulo = int(numero_capitulo[0])
-fimcapitulo = int(numero_capitulo[1])
+number_of_chapters = input('Enter a chapter interval separated by hifens: ')
+number_of_chapters = number_of_chapters.split('-',1)
+first_chapter = int(number_of_chapters[0])
+last_chapter = int(number_of_chapters[1])
 
 # Initialize url and make a soup
 manga = requests.get(link_anime)
 soup = BeautifulSoup(manga.text,'lxml')
 
 # Take the name of the manga that should be downloaded
-nome_anime = soup.body.h1.find_all(string=True) #Apanhar nome anime
-print(str(nome_anime[0]))
+manga_name = soup.body.h1.find_all(string=True) #Apanhar nome anime
+print(str(manga_name[0]))
 
 # Take the chapter links
-cap_manga = soup.find(class_= "chapter-list")
-capituloss = cap_manga.find_all('a') #Div Link dos capitulos
+manga_chapter = soup.find(class_= "chapter-list")
+chapters = manga_chapter.find_all('a') #Div Link dos capitulos
 
-capitulos_link = [] # The link of all chapters
+chapters_link = [] # The link of all chapters
 
-for capitulos in capituloss:
-    capitulos_link.append(str(capitulos.attrs['href'])) 
+for capitulos in chapters:
+    chapters_link.append(str(capitulos.attrs['href'])) 
 
-capitulos_link = capitulos_link[::-1]
+chapters_link = chapters_link[::-1]
 
 # Select range to download chapter
-chapter_chose = []
-chapter_chose = capitulos_link[comeco_capitulo:fimcapitulo]
+chosen_chapters = []
+chosen_chapters = chapters_link[first_chapter:last_chapter]
 
 # Download the chapters' images
-bar1 = Bar('Baixando', max = len(chapter_chose))
+bar1 = Bar('Downloading', max = len(chosen_chapters))
 count = 0
-for capitulo_link in chapter_chose:
+for chapter_link in chosen_chapters:
     count += 1
-    capitulo_links = requests.get(capitulo_link)
-    soup_capitulo = BeautifulSoup(capitulo_links.text,'lxml')
+    current_chapter_link = requests.get(chapter_link)
+    chapter_soup = BeautifulSoup(current_chapter_link.text,'lxml')
 
     # Find the images
-    class_imagem = soup_capitulo.find(class_= "vung-doc")
-    tag_imagem = class_imagem.find_all('img')
-    imagens = [] # The list of all images in the chapter
-    for img in tag_imagem:
-        imagens.append(str(img.attrs['src'])) 
+    image_class = chapter_soup.find(class_= "vung-doc")
+    image_tag = image_class.find_all('img')
+    images = [] # The list of all images in the chapter
+    for img in image_tag:
+        images.append(str(img.attrs['src'])) 
 
     # Downloading images
-    newpath = str(nome_anime[0]) + str(count) + '/'
+    newpath = str(manga_name[0]) + str(count) + '/'
     os.makedirs(newpath)
 
-    bar = Bar('Baixando', max = len(imagens))
-    contar_numero = 0
-    for imagem in imagens:
-        contar_numero += 1
-        numero = str(contar_numero)
-        full_path = newpath + str(nome_anime[0]) + numero + '.jpg'
-        response = requests.get(imagem, stream=True)
+    bar = Bar('Downloading', max = len(images))
+    counter = 0
+    for image in images:
+        counter += 1
+        number = str(counter)
+        full_path = newpath + str(manga_name[0]) + number + '.jpg'
+        response = requests.get(image, stream=True)
         with open(full_path, 'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
         del response
