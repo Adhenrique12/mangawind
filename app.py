ed2f12c997822bc8ec1491979cb61055018cb84b
Adelio Fonseca <adeliofonseca.lord@hotmail.com>
2019-12-12 01:24:11 -0100

    implement class

--- app.py
+++ app.py
@@ -5,44 +5,54 @@ from progress.bar import Bar # type: ignore
 import shutil
 from MangaManipulation import MangaManipulation 
 
-link_anime = MangaManipulation().run()
+class App:
 
-#Choose chapters
-number_of_chapters: list = input('Enter a chapter interval separated by hifens: ').split('-',1)
-first_chapter: int = int(number_of_chapters[0]) - 1
-if (len(number_of_chapters) == 1):
-    last_chapter: int = first_chapter
-else:
-    last_chapter = int(number_of_chapters[1]) - 1
+    def __init__(self, link_anime: str, number_of_chapters: list):
+        self.link_anime = link_anime
+        self.first_chapter = int(number_of_chapters[0]) - 1
+        if len(number_of_chapters) == 1:
+            self.last_chapter = self.first_chapter
+        else:
+            self.last_chapter = int(number_of_chapters[1])
 
-# Initialize url and make a soup
-manga = requests.get(link_anime)
-soup = BeautifulSoup(manga.text,'lxml')
+   
 
-# Take the name of the manga that should be downloaded
-manga_name: str = soup.body.h1.find_all(string=True) 
 
-# Take the chapter links
-manga_chapter = soup.find(class_="chapter-list")
-chapters = manga_chapter.find_all('a') #Div link of all manga chapters
+    @property
+    def soup(self):
+        return BeautifulSoup(requests.get(self.link_anime).text,'lxml')
+    @property
+    def manga_name(self):
+        return self.soup.body.h1.find_all(string=True)
 
-chapters_link: list = [] # The link of all chapters
+    @property
+    def manga_chapter(self):
+        return self.soup.find(class_="chapter-list").find_all('a')
 
-for manga_chapters in chapters:
-    chapters_link.append(str(manga_chapters.attrs['href'])) 
+    @property
+    def chapters_link(self):
+        self.link: list = []
+        for manga_chapter in self.manga_chapter:
+            self.link.append(str(manga_chapter.attrs['href']))
+        return  self.link[::-1]
+    @property
+    def chosen_chapters(self):
+        if self.manga_chapter[self.last_chapter] == self.manga_chapter[-1]:
+            return self.chapters_link[self.first_chapter:]
+        else:
+            return self.chapters_link[self.first_chapter:self.last_chapter + 1] 
 
-chapters_link = chapters_link[::-1]
 
-# Select range to download chapter
-chosen_chapters: list = []
 
-if chapters[last_chapter] == chapters[-1]:
-    chosen_chapters = chapters_link[first_chapter:]
-else:
-    chosen_chapters = chapters_link[first_chapter:last_chapter + 1]
+
+
+
+
+# Take the name of the manga that should be downloaded and the interval
+app = App(MangaManipulation().run(),input('Enter a chapter interval separated by hifens: ').split('-',1))
 
 # Download the chapters' images
-for chapter_link in chosen_chapters:
+for chapter_link in app.chosen_chapters:
     
     current_chapter_link = requests.get(chapter_link)
     chapter_soup = BeautifulSoup(current_chapter_link.text,'lxml')
@@ -58,7 +68,7 @@ for chapter_link in chosen_chapters:
         images.append(str(img.attrs['src'])) 
 
     # Downloading images
-    newpath: str = str(manga_name[0]) + '_' + str(chapter_number) + '/'
+    newpath: str = str(app.manga_name[0]) + '_' + str(chapter_number) + '/'
     try:
         os.makedirs(newpath)
     except:
@@ -77,6 +87,6 @@ for chapter_link in chosen_chapters:
         del response
         bar.next()
     os.chdir(newpath)
-    MangaManipulation.convert_to_pdf(str(manga_name[0]) + '_' + str(chapter_number))
+    MangaManipulation.convert_to_pdf(str(app.manga_name[0]) + '_' + str(chapter_number))
     os.chdir('..')
     bar.finish()
